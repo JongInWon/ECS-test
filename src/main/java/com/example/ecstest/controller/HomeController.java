95cac0c10667973391f13c778c4ce1503b2dc147
jongin <jonginwon1203@gmail.com>
2024-11-24 16:24:12 +0900

    fix

--- src/main/java/com/example/ecstest/controller/HomeController.java
+++ src/main/java/com/example/ecstest/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.example.ecstest.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1")
 public class HomeController {
 
-	@PostMapping("/")
+	@GetMapping("/")
 	public String save() {
 		return "Hello World!";
 	}
